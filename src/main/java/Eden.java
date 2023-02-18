910796b83df3e844e684b4eab56dcc9cc8d2ff8e
RiVeRx <petux3000@yandex.ru>
2023-02-19 00:28:55 +0500

    Started implementation of new parser
    Interpreter for numbers and simple math.

--- src/main/java/Eden.java
+++ src/main/java/Eden.java
@@ -11,6 +11,10 @@ import java.util.*;
  */
 public class Eden {
     static List<Token> tokenList = new ArrayList<>();
+    static Stack<EdenState> stackState = new Stack<>();
+    static Stack<Object> programStack = new Stack<>();
+    static int index = 0;
+    static boolean isInterpreter = true;
 
     public static void main(String[] args) throws IOException {
         // Args
@@ -44,13 +48,295 @@ public class Eden {
         lexer.tokenize();
         lexer.clearComments();
 
-        lexer.tokenList.forEach(System.out::println);
+        stackState.push(EdenState.PROGRAM);
+        Token currentToken = tokenList.get(index);
+        while (currentToken.type != TokenType.END || stackState.peek() != EdenState.PROGRAM) {
+            choseRule(currentToken);
+            currentToken = tokenList.get(index);
+        }
     }
 
-    static void printErr(Token token, String errMessage) {
+    static void choseRule(Token currentToken) {
+        EdenState state = stackState.pop();
+        switch (state) {
+            case PROGRAM: doStateProgram(); break;
+            case STATEMENT: doStateStatement(currentToken); break;
+            case PRINT_STATEMENT: doStatePrintStatement(); break;
+            case TOKEN_SEMICOLON: doTokenSemicolon(currentToken); break;
+            case TOKEN_CLOSE_BRACKET: doTokenCloseBracket(currentToken); break;
+            case EXPRESSION: doStateExpression(currentToken); break;
+            case LOGICAL: doStateLogical(currentToken); break;
+            case ADDITION: doStateAddition(currentToken); break;
+            case STARSLASH: doStateStarSlash(currentToken); break;
+            case UNAR: doStateUnar(currentToken); break;
+            case ARG: doStateArg(currentToken); break;
+            case DO_PRINT: doOpPrint(); break;
+            case DO_OP_PLUS: doOpPlus(); break;
+            case DO_OP_MINUS: doOpMinus(); break;
+            case DO_OP_MULTIPLY: doOpMultiply(); break;
+            case DO_OP_DIVIDE: doOpDivide(); break;
+            case DO_OP_UNAR_MINUS: doOpUnarMinus(); break;
+            case DO_SKIP: doStateSkip(currentToken); break;
+            default: {
+                System.err.printf("ERROR: Unknown State: %s with Token: %s", state, currentToken);
+                System.exit(-1);
+            }
+        }
+    }
+
+    static void doStateProgram() {
+        stackState.push(EdenState.PROGRAM);
+        stackState.push(EdenState.STATEMENT);
+    }
+
+    static void doStateStatement(Token currentToken) {
+        if (currentToken.type == TokenType.PRINT_STATEMENT) {
+            stackState.push(EdenState.PRINT_STATEMENT);
+            return;
+        }
+        printErrToken(currentToken, "Statement can't start with: ");
+    }
+
+    static void doStatePrintStatement() {
+        index++;
+        stackState.push(EdenState.DO_PRINT);
+        stackState.push(EdenState.TOKEN_SEMICOLON);
+        stackState.push(EdenState.EXPRESSION);
+    }
+
+    static void doTokenSemicolon(Token currentToken) {
+        if (currentToken.type == TokenType.SEMICOLON) {
+            index++;
+            return;
+        }
+        printErrToken(currentToken, "Expression must ends with [;], but found: ");
+    }
+
+    static void doTokenCloseBracket(Token currentToken) {
+        if (currentToken.type == TokenType.CLOSE_BRACKET) {
+            index++;
+            return;
+        }
+        printErrToken(currentToken, "Inner expression must ends with [)], but found: ");
+    }
+
+    static void doStateExpression(Token currentToken) {
+        TokenType cType = currentToken.type;
+        if (cType == TokenType.PLUS || cType == TokenType.MINUS
+                || cType == TokenType.OPEN_BRACKET || cType == TokenType.NUMBER) {
+            stackState.push(EdenState.LOGICAL);
+            stackState.push(EdenState.ADDITION);
+            stackState.push(EdenState.UNAR);
+            return;
+        }
+        printErrToken(currentToken, "Expression expects [+,-,NUMBER,(], but found: ");
+    }
+
+    static void doStateLogical(Token currentToken) {
+        TokenType cType = currentToken.type;
+        if (cType == TokenType.GREATER) {
+            index++;
+            stackState.push(EdenState.DO_OP_GREATER);
+            stackState.push(EdenState.ADDITION);
+            stackState.push(EdenState.UNAR);
+            return;
+        }
+        if (cType == TokenType.LESS) {
+            index++;
+            stackState.push(EdenState.DO_OP_LESS);
+            stackState.push(EdenState.ADDITION);
+            stackState.push(EdenState.UNAR);
+            return;
+        }
+        if (cType == TokenType.EQUALS) {
+            index++;
+            stackState.push(EdenState.DO_OP_EQUALS);
+            stackState.push(EdenState.ADDITION);
+            stackState.push(EdenState.UNAR);
+        }
+    }
+
+    static void doStateAddition(Token currentToken) {
+        TokenType cType = currentToken.type;
+        if (cType == TokenType.PLUS) {
+            index++;
+            stackState.push(EdenState.ADDITION);
+            stackState.push(EdenState.DO_OP_PLUS);
+            stackState.push(EdenState.UNAR);
+            return;
+        }
+        if (cType == TokenType.MINUS) {
+            index++;
+            stackState.push(EdenState.ADDITION);
+            stackState.push(EdenState.DO_OP_MINUS);
+            stackState.push(EdenState.UNAR);
+        }
+    }
+
+    static void doStateStarSlash(Token currentToken) {
+        TokenType cType = currentToken.type;
+        if (cType == TokenType.MULTIPLY) {
+            index++;
+            stackState.push(EdenState.STARSLASH);
+            stackState.push(EdenState.DO_OP_MULTIPLY);
+            stackState.push(EdenState.UNAR);
+            return;
+        }
+        if (cType == TokenType.DIVIDE) {
+            index++;
+            stackState.push(EdenState.STARSLASH);
+            stackState.push(EdenState.DO_OP_DIVIDE);
+            stackState.push(EdenState.UNAR);
+        }
+        // TODO: ADD POWER OPERATION (^)
+    }
+
+    static void doStateUnar(Token currentToken) {
+        if (currentToken.type == TokenType.PLUS) {
+            index++;
+            stackState.push(EdenState.DO_OP_UNAR_PLUS);
+            stackState.push(EdenState.STARSLASH);
+            stackState.push(EdenState.ARG);
+            return;
+        }
+        if (currentToken.type == TokenType.MINUS) {
+            index++;
+            stackState.push(EdenState.DO_OP_UNAR_MINUS);
+            stackState.push(EdenState.STARSLASH);
+            stackState.push(EdenState.ARG);
+            return;
+        }
+        if (currentToken.type == TokenType.NUMBER || currentToken.type == TokenType.OPEN_BRACKET) {
+            stackState.push(EdenState.STARSLASH);
+            stackState.push(EdenState.ARG);
+        }
+    }
+
+    static void doStateArg(Token currentToken) {
+        if (currentToken.type == TokenType.NUMBER) {
+            stackState.push(EdenState.DO_SKIP);
+            return;
+        }
+        if (currentToken.type == TokenType.OPEN_BRACKET) {
+            index++;
+            stackState.push(EdenState.TOKEN_CLOSE_BRACKET);
+            stackState.push(EdenState.EXPRESSION);
+            // Do not forget put return here, when add more code below
+        }
+    }
+
+    static void doStateSkip(Token currentToken) {
+        if (isInterpreter) {
+             if (currentToken.type == TokenType.NUMBER) {
+                 index++;
+                 programStack.push(currentToken.value);
+             }
+        }
+    }
+
+    static void doOpPrint() {
+        if (isInterpreter) {
+            if (programStack.size() < 1) {
+                printErr("Nothing to print");
+            }
+            Object value = programStack.pop();
+            System.out.println(value);
+        }
+    }
+
+    static void doOpPlus() {
+        if (isInterpreter) {
+            if (programStack.size() < 2) {
+                printErr("Plus operation expected two integers, but found less");
+            }
+            Object first = programStack.pop();
+            Object second = programStack.pop();
+            int result = Integer.parseInt(String.valueOf(first)) + Integer.parseInt(String.valueOf(second));
+            programStack.push(result);
+        }
+    }
+
+    static void doOpMinus() {
+        if (isInterpreter) {
+            if (programStack.size() < 2) {
+                printErr("Minus operation expected two integers, but found less");
+            }
+            Object second = programStack.pop();
+            Object first = programStack.pop();
+            int result = Integer.parseInt(String.valueOf(first)) - Integer.parseInt(String.valueOf(second));
+            programStack.push(result);
+        }
+    }
+
+    static void doOpMultiply() {
+        if (isInterpreter) {
+            if (programStack.size() < 2) {
+                printErr("Multiply operation expected two integers, but found less");
+            }
+            Object first = programStack.pop();
+            Object second = programStack.pop();
+            int result = Integer.parseInt(String.valueOf(first)) * Integer.parseInt(String.valueOf(second));
+            programStack.push(result);
+        }
+    }
+
+    static void doOpDivide() {
+        if (isInterpreter) {
+            if (programStack.size() < 2) {
+                printErr("Divide operation expected two integers, but found less");
+            }
+            Object second = programStack.pop();
+            Object first = programStack.pop();
+            int result = Integer.parseInt(String.valueOf(first)) / Integer.parseInt(String.valueOf(second));
+            programStack.push(result);
+        }
+    }
+
+    static void doOpUnarMinus() {
+        if (isInterpreter) {
+            if (programStack.size() < 1) {
+                printErr("Unar minus operation expected integer, but found nothing");
+            }
+            Object first = programStack.pop();
+            int result = Integer.parseInt(String.valueOf(first)) * -1;
+            programStack.push(result);
+        }
+    }
+
+    static void printErrToken(Token token, String errMessage) {
         int offset = String.valueOf(token.value).length();
         System.err.printf("ERROR: [%d:%d] %s: (%s)'%s'%n", token.loc.line + 1, token.loc.column - offset, errMessage, token.type, token.value);
         System.exit(1);
+    }
+
+    static void printErr(String errMessage) {
+        System.err.printf("ERROR: %s", errMessage);
+        System.exit(2);
+    }
+
+    enum EdenState {
+        PROGRAM,
+        STATEMENT,
+        PRINT_STATEMENT,
+        DO_PRINT,
+        DO_SKIP,
+        EXPRESSION,
+        DO_OP_PLUS,
+        DO_OP_MINUS,
+        DO_OP_UNAR_PLUS,
+        DO_OP_UNAR_MINUS,
+        DO_OP_MULTIPLY,
+        DO_OP_DIVIDE,
+        DO_OP_GREATER,
+        DO_OP_LESS,
+        DO_OP_EQUALS,
+        TOKEN_CLOSE_BRACKET,
+        TOKEN_SEMICOLON,
+        LOGICAL,
+        ADDITION,
+        STARSLASH,
+        UNAR,
+        ARG
     }
 
     enum TokenType {
@@ -64,7 +350,7 @@ public class Eden {
         PRINT_STATEMENT,
         PLUS,
         MINUS,
-        STAR,
+        MULTIPLY,
         DIVIDE,
         GREATER,
         LESS,
@@ -211,7 +497,7 @@ public class Eden {
 
         void tokenizeSpecialCharacters() {
             if (allowedCharacters.indexOf(currentChar) == -1) {
-                printErr(new Token(TokenType.SYMBOL, currentChar, new Location(line, column)), "Lexer: Symbol is not allowed");
+                printErrToken(new Token(TokenType.SYMBOL, currentChar, new Location(line, column)), "Lexer: Symbol is not allowed");
             }
             switch (currentChar) {
                 case ';': {
@@ -247,7 +533,7 @@ public class Eden {
                     break;
                 }
                 case '*': {
-                    tokenList.add(new Token(TokenType.STAR, currentChar, new Location(line, column)));
+                    tokenList.add(new Token(TokenType.MULTIPLY, currentChar, new Location(line, column)));
                     break;
                 }
                 case '/': {
@@ -271,7 +557,7 @@ public class Eden {
                     break;
                 }
                 default: {
-                    printErr(new Token(TokenType.SYMBOL, currentChar, new Location(line, column)), "Lexer: Unknown character");
+                    printErrToken(new Token(TokenType.SYMBOL, currentChar, new Location(line, column)), "Lexer: Unknown character");
                 }
             }
             next();
